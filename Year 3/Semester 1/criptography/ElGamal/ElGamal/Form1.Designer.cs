26740f1984584bec91f9ad05c7be9fe4d034b479
Denis <denis_vieriu@yahoo.com>
2019-01-15 00:13:23 +0200

    Add elGammal Cripto

---
+++ Year 3/Semester 1/criptography/ElGamal/ElGamal/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace ElGamal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.decrypt_button = new System.Windows.Forms.Button();
+            this.encrypt_text_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.plainText = new System.Windows.Forms.TextBox();
+            this.beta = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.alfa = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // decrypt_button
+            // 
+            this.decrypt_button.Location = new System.Drawing.Point(257, 197);
+            this.decrypt_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.decrypt_button.Name = "decrypt_button";
+            this.decrypt_button.Size = new System.Drawing.Size(84, 88);
+            this.decrypt_button.TabIndex = 1;
+            this.decrypt_button.Text = "Decrypt";
+            this.decrypt_button.UseVisualStyleBackColor = true;
+            this.decrypt_button.Click += new System.EventHandler(this.decrypt_button_Click);
+            // 
+            // encrypt_text_button
+            // 
+            this.encrypt_text_button.Location = new System.Drawing.Point(257, 67);
+            this.encrypt_text_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.encrypt_text_button.Name = "encrypt_text_button";
+            this.encrypt_text_button.Size = new System.Drawing.Size(84, 88);
+            this.encrypt_text_button.TabIndex = 2;
+            this.encrypt_text_button.Text = "Encrypt";
+            this.encrypt_text_button.UseVisualStyleBackColor = true;
+            this.encrypt_text_button.Click += new System.EventHandler(this.encrypt_text_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(458, 286);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Laboratory three";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 41);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Plain Text";
+            // 
+            // plainText
+            // 
+            this.plainText.Location = new System.Drawing.Point(27, 67);
+            this.plainText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.plainText.Multiline = true;
+            this.plainText.Name = "plainText";
+            this.plainText.Size = new System.Drawing.Size(208, 88);
+            this.plainText.TabIndex = 6;
+            // 
+            // beta
+            // 
+            this.beta.Location = new System.Drawing.Point(135, 219);
+            this.beta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.beta.Name = "beta";
+            this.beta.Size = new System.Drawing.Size(100, 20);
+            this.beta.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(458, 314);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 18);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "ElGamal";
+            // 
+            // alfa
+            // 
+            this.alfa.Location = new System.Drawing.Point(135, 197);
+            this.alfa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.alfa.Name = "alfa";
+            this.alfa.Size = new System.Drawing.Size(100, 20);
+            this.alfa.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(98, 200);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "alfa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(100, 223);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "beta";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.alfa);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.beta);
+            this.Controls.Add(this.plainText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.encrypt_text_button);
+            this.Controls.Add(this.decrypt_button);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button decrypt_button;
+        private System.Windows.Forms.Button encrypt_text_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox plainText;
+        private System.Windows.Forms.TextBox beta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox alfa;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Year 3/Semester 1/criptography/ElGamal/ElGamal/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace ElGamal
+{
+    public partial class Form1 : Form
+    {
+        /* 
+        * Alphabet:
+        *  _ A B C D E F G H I J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z
+        *  0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
+        */
+
+        BigInteger p, g, a;
+        const int MAX_PRIME = 20000;
+        const int ALPHABET_SIZE = 27;
+
+        int kP = 2; // size of a plaintext chunk
+        int lC = 3; // size of a plaintext chunk
+
+        public void GenerateKeys()
+        {
+            // 1. Key generation. Alice creates a public key and a private key
+
+            // 1.1. Generates a large random prime p and a generator g of (Zp *, .)
+            p = new BigInteger(generateRandomPrime(MAX_PRIME));
+
+            // 1.2. Selects a random integer a (1 ≤ a ≤ p − 2).
+            a = generateRandomBigInteger(1, p - 2);
+
+            // 1.3. Computes g^a mod p
+            g = new BigInteger(bruteForceGenerator((int)p));
+
+            MessageBox.Show("p = " + p + ", a = " + a + ", g = " + g);
+            // 1.4. Alice’s public key is (p, g, g^a); her private key is a
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 1. Key generation
+            GenerateKeys();
+        }
+
+        public BigInteger generateRandomBigInteger(BigInteger left, BigInteger right)
+        {
+            BigInteger a;
+            RandomNumberGenerator rng = RandomNumberGenerator.Create();
+            byte[] bytes = new byte[BigInteger.Divide(right, 2).ToByteArray().LongLength];
+            do
+            {
+                rng.GetBytes(bytes);
+                a = new BigInteger(bytes);
+            } while (a < left || a >= right - 2);
+
+            return a;
+
+        }
+
+        private int RandomFromList(List<int> lst)
+        {
+            Random rnd = new Random();
+            return lst[rnd.Next(0, lst.Count)];
+        }
+
+        private int bruteForceGenerator(int p)
+        {
+            var r = from g in Enumerable.Range(2, p - 2).AsParallel()
+                    where Enumerable.Range(1, p - 3).All(i => BigInteger.ModPow(g, i, p) != 1)
+                    select g;
+
+            return RandomFromList(r.ToList());
+        }
+
+        public int generateRandomPrime(int maxLimit)
+        {
+            int lowBound = (int)Math.Pow(27, kP);
+            int highBound = (int)Math.Pow(27, lC);
+
+            var r = from i in Enumerable.Range(lowBound, highBound - 1).AsParallel()
+                    where Enumerable.Range(2, (int)Math.Sqrt(i)).All(j => i % j != 0)
+                    select i;
+
+            return RandomFromList(r.ToList());
+    }
+
+        public Tuple<BigInteger, string> encryptMessage(String s)
+        {
+            // 2. Encryption. Bob sends an encrypted message to Alice.
+            BigInteger k, alpha, beta;
+            string betaAsString = "";
+
+            int pos;
+            char c;
+
+            // 2.1 Gets Alice’s public key (p, g, g^a).
+            // 2.3. Selects a random integer k (1 ≤ k ≤ p − 2).
+            k = generateRandomBigInteger(1, p - 2);
+
+            // 2.4. Computes alpha = g^k mod p and β = m * ((g^a)^k) mod
+            alpha = BigInteger.ModPow(g, k, p);
+
+            // 2.2. Represents the message as a number m between 0 and p − 1.
+            // Plaintext - chunks of 2 letters
+            // Cipertext - chunks of 3 letters
+            // -------------------------------------------------------------------------------------------------------
+
+            for (int i = 0; i < s.Length; i += 2)
+            {
+                int sumChunks = 0;
+                for (int j = 0; j < kP; j++)
+                {
+                    if (i + j >= s.Length)
+                    {
+                        c = ' ';
+                    }
+                    else
+                    {
+                        c = s[i + j];
+                    }
+
+                    pos = (c == ' ') ? (0) : (c - 'a' + 1);
+                    sumChunks += pos * (int)Math.Pow(ALPHABET_SIZE, kP - j - 1);
+                }
+
+                beta = BigInteger.ModPow(BigInteger.Multiply(sumChunks, BigInteger.ModPow(BigInteger.ModPow(g, a, p), k, p)), 1, p);
+                int coeff = 0;
+                string outputAsText = "";
+                char cryptedChar;
+
+                int betaAsInt = (int)beta;
+                for (int z = 0; z < lC; z++)
+                {
+                    coeff = betaAsInt % ALPHABET_SIZE;
+                    cryptedChar = (coeff == 0) ? ((char)(' ')) : ((char)('a' + coeff - 1));
+                    betaAsInt = betaAsInt / ALPHABET_SIZE;
+
+                    outputAsText = cryptedChar + outputAsText;
+                }
+                betaAsString += outputAsText;
+            }
+            // -------------------------------------------------------------------------------------------------------
+
+            // 2.5 Sends the ciphertext c = (alhpa, Beta) to Alice
+            return new Tuple<BigInteger, string>(alpha, betaAsString);
+        }
+
+        public string decryptMessage(BigInteger alpha, string betaText)
+        {
+            BigInteger exp,t1 = 1, t2;
+            int pos;
+            char c;
+            string decryptedText = "";
+
+            //exp = BigInteger.Subtract(BigInteger.Subtract(p, 1), a);
+            //while (exp != 0)
+            //{
+            //    t1 = BigInteger.Multiply(t1, alpha);
+            //    exp = BigInteger.Subtract(exp, 1);
+            //}
+
+            BigInteger lhs = BigInteger.ModPow(alpha, p - 1 - a, p);
+            BigInteger res;
+            for (int i = 0; i < betaText.Length; i += 3)
+            {
+                int sumChunks = 0;
+                for (int j = 0; j < lC; j++)
+                {
+                    if (i + j >= betaText.Length)
+                    {
+                        c = ' ';
+                    }
+                    else
+                    {
+                        c = betaText[i + j];
+                    }
+
+                    pos = (c == ' ') ? (0) : (c - 'a' + 1);
+                    sumChunks += pos * (int)Math.Pow(ALPHABET_SIZE, lC - j - 1);
+
+                }
+
+                res = BigInteger.Multiply(lhs, sumChunks);
+                res = BigInteger.ModPow(res, 1, p);
+
+                int coeff = 0;
+                string outputAsText = "";
+                char cryptedChar;
+
+                int betaAsInt = (int)res;
+                for (int z = 0; z < kP; z++)
+                {
+                    coeff = betaAsInt % ALPHABET_SIZE;
+                    cryptedChar = (coeff == 0) ? ((char)(' ')) : ((char)('a' + coeff - 1));
+                    betaAsInt = betaAsInt / ALPHABET_SIZE;
+
+                    outputAsText = cryptedChar + outputAsText;
+                }
+                decryptedText += outputAsText;
+            }
+
+            return decryptedText;
+        }
+
+        public Boolean validateText(String s)
+        {
+            foreach (char c in s)
+            {
+                if (((c < 'a') || (c > 'z')) && (c != ' '))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public Boolean validateEncryptedText(String s)
+        {
+            foreach (char c in s)
+            {
+                if (((c < 'A') || (c > 'Z')) && (c != ' '))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public int posInAlphabet(char c)
+        {
+            int pos;
+
+            if (c == ' ')
+            {
+                pos = 0;
+            }
+            else
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    pos = (c - 'a') + 1;
+                }
+                else
+                {
+                    pos = (c - 'A') + 1;
+                }
+            }
+
+            return pos;
+        }
+
+
+        private void encrypt_text_button_Click(object sender, EventArgs e)
+        {
+            String text = this.plainText.Text.ToString();
+            if (this.validateText(text) == false)
+            {
+                MessageBox.Show("The plain text should be lower case");
+                MessageBox.Show("Also it should only have letters");
+            }
+            else
+            {
+
+                String input = this.plainText.Text;
+                MessageBox.Show("Input message: " + input);
+                Tuple<BigInteger, string> encrypted = this.encryptMessage(input);
+                this.alfa.Text = encrypted.Item1.ToString();
+                this.beta.Text = encrypted.Item2.ToString();
+            }
+        }
+
+        private void decrypt_button_Click(object sender, EventArgs e)
+        {
+            string res = this.decryptMessage(BigInteger.Parse(this.alfa.Text), this.beta.Text);
+            this.plainText.Text = res.ToUpper().ToString();
+        }
+    }
+}
